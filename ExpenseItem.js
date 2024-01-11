4d644f3a0a9221f08769edded5fe2f119fbf5673
sujeetkumarSCE <90716564+sujeetkumarSCE@users.noreply.github.com>
2024-01-11 08:27:56 +0530

    Add files via upload

---
+++ ExpenseItem.js
@@ -0,0 +1,9 @@
+function ExpenseItem() {
+    return <h2>Expense item!
+        <h3>Food Rs 10</h3>
+        <h3>Petrol Rs 100</h3>
+        <h3>Movies Rs 200</h3>
+    </h2>
+}
+
+export default ExpenseItem;
