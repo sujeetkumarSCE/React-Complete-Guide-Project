19c80a33ff65576cb64e7190b8c984bafc00214e
sujeetkumarSCE <90716564+sujeetkumarSCE@users.noreply.github.com>
2024-01-12 08:14:51 +0530

    Styling and using JS in JSX for Expense app

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,9 +1,15 @@
+import './ExpenseItem.css';
 function ExpenseItem() {
-    return <h2>Expense item!
-        <h3>Food Rs 10</h3>
-        <h3>Petrol Rs 100</h3>
-        <h3>Movies Rs 200</h3>
-    </h2>
+    return (
+<div className="expense-item">
+<div>March 28th 2024</div>
+<div className="expense-item__description">
+    <h2>Car Insurance</h2>
+    <div className="expense-item__price">$294.67</div>
+</div>   
+</div> 
+    
+    );   
 }
 
 export default ExpenseItem;
