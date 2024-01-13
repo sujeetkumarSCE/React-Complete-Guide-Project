32ce7fa0925b7f25e9fc7a8a4495c6477f1d66fa
sujeetkumarSCE <90716564+sujeetkumarSCE@users.noreply.github.com>
2024-01-13 07:19:04 +0530

    Making Components Smaller and Readable

--- App.js
+++ App.js
@@ -46,31 +46,31 @@ function App() {
        amount={expenses[0].amount}
        location={expenses[0].location}
        date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
       title={expenses[1].title}
       amount={expenses[1].amount}
       location={expenses[1].location}
       date={expenses[1].date} 
-      ></ExpenseItem>
+      />
       <ExpenseItem
       title={expenses[2].title}
       amount={expenses[2].amount}
       location={expenses[2].location}
       date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
       title={expenses[3].title}
       amount={expenses[3].amount}
       location={expenses[3].location}
       date={expenses[3].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
       title={expenses[4].title}
       amount={expenses[4].amount}
       location={expenses[4].location}
       date={expenses[4].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
