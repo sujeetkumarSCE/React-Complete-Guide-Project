4599e6d654a83456f260e11ea397027d11b11037
sujeetkumarSCE <90716564+sujeetkumarSCE@users.noreply.github.com>
2024-01-20 17:09:03 +0530

    Design patterns with state

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -4,27 +4,55 @@ const ExpenseForm = () => {
    const [enteredTitle, setEnteredTitle] = useState('');
    const [enteredAmount, setEnteredAmount] = useState('');
    const [enteredDate, setEnteredDate] = useState('');
+    // const [userInput, setUserInput] = useState({
+    //    enteredTitle: '',
+    //    enteredAmount: '',
+    //    enteredDate: '' 
+    // });
 
   const titleChangeHandler = (event) => {
    setEnteredTitle(event.target.value);
+   //setUserInput({
+    //...userInput,
+    //  enteredTitle: event.target.value,
+   //});
+//    setUserInput((prevState) => {
+//     return {...prevState, enteredTitle: event.target.value};
+//    });
   };
 
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //       enteredAmount: event.target.value,
+    //    });
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //       enteredDate: event.target.value,
+    //    });
   };
-
+   const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+     title: enteredTitle,
+     amount: enteredAmount,
+     date: new Date(enteredDate)
+    };
+     console.log(expenseData);
+   };
     return (
-    <form>
+    <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
                 <input type="text" onChange={titleChangeHandler}/>
             </div>
-            <div className='new-expense-control'>
+            <div className='new-expense__control'>
                 <label>Amount</label>
                 <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}/>
             </div>
@@ -34,7 +62,7 @@ const ExpenseForm = () => {
             </div>
         </div>
         <div className="new-expense__actions">
-            <button type="submit">Add Expense</button>
+            <button type="submit" >Add Expense</button>
         </div>
     </form>
     );
